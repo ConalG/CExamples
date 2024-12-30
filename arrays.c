3885a1ee9f16e814d4491284f808fcbb4c3bfd30
Conal Glynnmartin <conal-glynnmartin@hotmail.com>
2024-12-30 14:07:50 +0000

    Modified arrays.c

--- arrays.c
+++ arrays.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
+	/*
 	int marks[5];
 
 	for(int i = 0; i < 5; ++i ){
@@ -13,4 +14,16 @@ int main(){
 	int average = sum/5;
 					
 	printf("Average class mark: %d\n", average);
+	*/
+
+//	Find the largest number in the array
+
+	int arr[] = {34,12,21,54,48};
+	int largest = *arr;
+	for(int i = 0; i < 5; ++i){
+		if(largest < *(arr+i)){
+			largest = *(arr+i);
+			}
+		}
+	printf("Largest number is: %d\n", largest);
 	}
